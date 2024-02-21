496d0d75037ec71be808aad0dfbf83f5bffdc1b5
Sebastian <sebaso201619@gmail.com>
2024-02-21 13:21:06 -0500

    Building-Interactive-Apps

--- app/src/main/java/co/edu/unipiloto/MainActivity.java
+++ app/src/main/java/co/edu/unipiloto/MainActivity.java
@@ -13,14 +13,15 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     TextView estado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        estado=findViewById(R.id.textView);
+        estado = findViewById(R.id.textView);
         Spinner spinner = findViewById(R.id.spinner1);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.products, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.products, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
@@ -29,34 +30,38 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @SuppressLint("SetTextI18n")
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text= parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), "Seleccionado"+parent.getItemAtPosition(position),Toast.LENGTH_SHORT).show();
-        if(parent.getItemAtPosition(position).equals("Solido")){
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), "Seleccionado " + text, Toast.LENGTH_SHORT).show();
+
         Spinner spinner2 = findViewById(R.id.spinner2);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.productsSolido, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner2.setAdapter(adapter);
-        estado.setText("Seleccionado: "+parent.getItemAtPosition(position).toString()+" "+spinner2.getSelectedItem().toString());
-        }
-        else if(parent.getItemAtPosition(position).equals("Agricola")){
-            Spinner spinner2 = findViewById(R.id.spinner2);
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.productsAgricola, android.R.layout.simple_spinner_item);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            spinner2.setAdapter(adapter);
-            estado.setText("Seleccionado: "+parent.getItemAtPosition(position).toString()+" "+spinner2.getSelectedItem().toString());
-        }
-        else if (parent.getItemAtPosition(position).equals("Liquidos")){
-        Spinner spinner2 = findViewById(R.id.spinner2);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.productsLiquido, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner2.setAdapter(adapter);
-            estado.setText("Seleccionado: "+parent.getItemAtPosition(position).toString()+" "+spinner2.getSelectedItem().toString());
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent2, View view2, int position2, long id2) {
+                estado.setText("Seleccionado: " + parent.getItemAtPosition(position).toString() + " "
+                        + parent2.getItemAtPosition(position2).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent2) {
+            }
+        });
+
+        if (text.equals("Solido")) {
+            ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.productsSolido, android.R.layout.simple_spinner_item);
+            adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner2.setAdapter(adapter2);
+        } else if (text.equals("Agricola")) {
+            ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.productsAgricola, android.R.layout.simple_spinner_item);
+            adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner2.setAdapter(adapter2);
+        } else if (text.equals("Liquidos")) {
+            ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.productsLiquido, android.R.layout.simple_spinner_item);
+            adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner2.setAdapter(adapter2);
         }
     }
 
-
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-
     }
 }
